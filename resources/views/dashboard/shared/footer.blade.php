d52a13e7bf10b6bd7fa34afb1c9eab7162714a5d
Erick Christian <erickchristian48@gmail.com>
2021-03-02 23:14:40 +0700

    Revert "I'm sorry pls idk anything about laravel"
    This reverts commit 8231531529185905d239805f10f106b3d4a37863.

--- resources/views/dashboard/shared/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="c-footer">
-  <div><a href="https://coreui.io">CoreUI</a> &copy; 2020 creativeLabs.</div>
-  <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::view('/dashboard', 'dashboard.homepage');
