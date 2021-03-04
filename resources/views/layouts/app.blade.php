2d205d1bca7b2aa38a2ea55422744f9686039b5c
Erick Christian <erickchristian48@gmail.com>
2021-03-04 21:28:54 +0700

    task: add material icons cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>{{ config('app.name') }}</title>
@@ -8,9 +9,9 @@
     <!-- CoreUI CSS -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-          crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     @yield('third_party_stylesheets')
 
@@ -18,30 +19,31 @@
 </head>
 
 <body class="c-app">
-@include('layouts.sidebar')
+    @include('layouts.sidebar')
 
-<div class="c-wrapper">
-    <header class="c-header c-header-light c-header-fixed">
-        @include('layouts.header')
-    </header>
+    <div class="c-wrapper">
+        <header class="c-header c-header-light c-header-fixed">
+            @include('layouts.header')
+        </header>
 
-    <div class="c-body">
-        <main class="c-main">
-            @yield('content')
-        </main>
+        <div class="c-body">
+            <main class="c-main">
+                @yield('content')
+            </main>
+        </div>
+
+        <footer class="c-footer">
+            <div><a href="https://coreui.io">CoreUI</a> © 2020 creativeLabs.</div>
+            <div class="mfs-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
+        </footer>
     </div>
 
-    <footer class="c-footer">
-        <div><a href="https://coreui.io">CoreUI</a> © 2020 creativeLabs.</div>
-        <div class="mfs-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
-    </footer>
-</div>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/perfect-scrollbar.js"></script>
 
-<script src="{{ mix('js/app.js') }}" defer></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/perfect-scrollbar.js"></script>
+    @yield('third_party_scripts')
 
-@yield('third_party_scripts')
-
-@stack('page_scripts')
+    @stack('page_scripts')
 </body>
+
 </html>
