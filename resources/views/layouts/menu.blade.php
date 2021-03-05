e5f5f2d50a504800aefaab0495206767964582a3
Marchella Putri Sannie <marchella.putris2@gmail.com>
2021-03-05 09:19:47 +0700

    CRUD Guru dan Rayon

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,15 +6,21 @@
 
 <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle"
         href="#">
-        <i class="c-sidebar-nav-icon cil-diamond"></i> Dropdown</a>
+        <i class="c-sidebar-nav-icon cil-diamond"></i> Input Data</a>
     <ul class="c-sidebar-nav-dropdown-items">
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ url('/guru') }}"><span
+                    class="c-sidebar-nav-icon"></span> Data Guru</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ url('/rayon')}}"><span
+                    class="c-sidebar-nav-icon"></span> Data Rayon</a></li>
         <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
-                    class="c-sidebar-nav-icon"></span> Buttons</a></li>
+                    class="c-sidebar-nav-icon"></span> Data Jurusan</a></li>
         <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
-                    class="c-sidebar-nav-icon"></span> Buttons Group</a></li>
+                    class="c-sidebar-nav-icon"></span> Data Siswa</a></li>
         <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
-                    class="c-sidebar-nav-icon"></span> Dropdowns</a></li>
+                    class="c-sidebar-nav-icon"></span> Data Mapel</a></li>
         <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
-                    class="c-sidebar-nav-icon"></span> Brand Buttons</a></li>
+                    class="c-sidebar-nav-icon"></span> Data Absen</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
+                    class="c-sidebar-nav-icon"></span> Data UPD</a></li>
     </ul>
 </li>
