5b01974780f9a8c794ea7dd4927fd7c84e616c01
Marchella Putri Sannie <marchella.putris2@gmail.com>
2021-03-06 11:45:13 +0700

    Update crud upd + login

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -15,3 +15,4 @@ Route::resource('rayon', 'RayonController');
 Route::resource('mapel', 'MapelController');
 Route::resource('siswa', 'SiswaController');
 Route::resource('absen', 'AbsenController');
+Route::resource('upd', 'UpdController');
