066a1c579ce72232477afd824d4512f45cecde0c
Marchella Putri Sannie <marchella.putris2@gmail.com>
2021-03-07 21:56:08 +0700

    Create CRUD Detail UPD

---
+++ app/Detail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Detail extends Model
+{
+    protected $table = "detail_upd";
+    protected $fillable = ['nama_upd','guru_id'];
+
+    public function guru()
+    {
+	    return $this->belongsTo('App\Guru');
+    }
+
+    public function upd()
+    {
+        return $this->hasMany('App\Upd');
+    }
+}
